7972e53f9d31fc9ed0af6765501bd5e575f009f8
mczhuge <mczhuge@gmail.com>
2023-05-23 22:30:34 +0300

    update

---
+++ society/skin_retouching/agent.py
@@ -0,0 +1,42 @@
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+from modelscope.outputs import OutputKeys
+import torch
+from PIL import Image
+import os
+import uuid
+import cv2
+
+def prompts(name, description):
+    def decorator(func):
+        func.name = name
+        func.description = description
+        return func
+
+    return decorator
+
+
+class ABPN:
+    def __init__(self, device):
+        print(f"Initializing ABPN to {device}")
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        model_id = 'damo/cv_unet_skin-retouching'
+        self.pipeline_skin_retouching = pipeline(Tasks.skin_retouching, model=model_id)
+
+    @prompts(name="ABPN (Skin Retouching)",
+             description="Useful when you want to make the face in the photo more beautiful. Receives image_path as input."
+                         "Applications involving skin beautification, such as photo retouching."
+                         "The input to this tool should be a string, representing the image_path.")
+    def inference(self, image_path):
+        
+        image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+        image_path = image_path.strip("\n")
+        result =  self.pipeline_skin_retouching(image_path)
+        cv2.imwrite(image_filename, result[OutputKeys.OUTPUT_IMG])
+        return image_filename
+    
+
+if __name__ == "__main__":
+    skin_touching_model = ABPN(device="cuda:0")
+    image = skin_touching_model.inference("WechatIMG895.jpeg")
