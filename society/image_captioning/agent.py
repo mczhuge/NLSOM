3d1684cd5152e7da7cc2c8bae192b4401bcee3c1
mczhuge <mczhuge@gmail.com>
2023-05-23 21:36:47 +0300

    update

---
+++ society/image_captioning/agent.py
@@ -0,0 +1,95 @@
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+from modelscope.outputs import OutputKeys
+
+
+import torch
+from PIL import Image
+import requests
+from transformers import Blip2Processor, Blip2ForConditionalGeneration
+import os
+
+def prompts(name, description):
+    def decorator(func):
+        func.name = name
+        func.description = description
+        return func
+
+    return decorator
+
+class mPLUG_captioning:
+    def __init__(self, device):
+        print(f"Initializing mPLUG_captioning to {device}")
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        model_id = 'damo/mplug_image-captioning_coco_large_en'
+        self.pipeline_caption = pipeline(Tasks.image_captioning, model=model_id)
+
+    @prompts(name="mPLUG_captioning",
+             description="Useful when you want to know what is inside the photo. Receives image_path as input. "
+                         "If there are other captioning methods, it is also suggested to utilize other captioning methods to better know the image."
+                         "The input to this tool should be a string, representing the image_path. ")
+    def inference(self, image_path):
+        image_path = image_path.strip("\n")
+        captions = self.pipeline_caption(image_path)
+        print(f"\nProcessed mPLUG_captioning, Input Image: {image_path}, Output Text: {captions}")
+        return captions
+    
+
+class OFA_distilled_captioning:
+    def __init__(self, device):
+        print(f"Initializing OFA_distilled_captioning to {device}")
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        model_id = 'damo/ofa_image-caption_coco_large_en'
+        self.pipeline_caption = pipeline(Tasks.image_captioning, model=model_id, model_revision='v1.0.1')
+
+    @prompts(name="OFA_distilled_captioning",
+             description="Useful when you want to know what is inside the photo. Receives image_path as input. "
+                         "If there are other captioning methods, it is also suggested to utilize other captioning methods to better know the image."
+                         "The input to this tool should be a string, representing the image_path. ")
+    def inference(self, image_path):
+        image_path = image_path.strip("\n")
+        captions = self.pipeline_caption(image_path)[OutputKeys.CAPTION]
+        #print(f"\nProcessed mPLUG_captioning, Input Image: {image_path}, Output Text: {captions}")
+        return captions
+    
+class OFA_large_captioning:
+    def __init__(self, device):
+        print(f"Initializing OFA_large_captioning to {device}")
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        model_id = 'damo/ofa_image-caption_coco_large_en'
+        self.pipeline_caption = pipeline(Tasks.image_captioning, model=model_id, model_revision='v1.0.1')
+
+    @prompts(name="OFA_large_captioning",
+             description="Useful when you want to know what is inside the photo. Receives image_path as input. "
+                         "If there are other captioning methods, it is also suggested to utilize other captioning methods to better know the image."
+                         "The input to this tool should be a string, representing the image_path. ")
+    def inference(self, image_path):
+        image_path = image_path.strip("\n")
+        captions = self.pipeline_caption(image_path)[OutputKeys.CAPTION]
+        #print(f"\nProcessed mPLUG_captioning, Input Image: {image_path}, Output Text: {captions}")
+        return captions
+    
+class BLIP2_captioning:
+    def __init__(self, device):
+        print(f"Initializing BLIP2 to {device}")
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        model_id = 'Salesforce/blip2-flan-t5-xl'
+        self.processor = Blip2Processor.from_pretrained(model_id)
+        self.BLIP2_MODEL = Blip2ForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16)
+
+    @prompts(name="BLIP2_captioning",
+             description="Useful when you want to know what is inside the photo. Receives image_path as input."
+                         "If there are other captioning methods, it is also suggested to utilize other captioning methods to better know the image."
+                         "The input to this tool should be a string, representing the image_path. ")
+    def inference(self, input):
+
+        raw_image = Image.open(input).convert("RGB")
+        inputs = self.processor(images=raw_image, return_tensors="pt").to("cuda", torch.float16)
+        generated_answer_ids = self.BLIP2_MODEL.generate(**inputs)
+        answer = self.processor.batch_decode(generated_answer_ids, skip_special_tokens=True)[0].strip()
+
+        return answer
---
+++ society/image_colorization/agent.py
@@ -0,0 +1,60 @@
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+from modelscope.outputs import OutputKeys
+import torch
+from PIL import Image
+import os
+import uuid
+import cv2
+
+def prompts(name, description):
+    def decorator(func):
+        func.name = name
+        func.description = description
+        return func
+
+    return decorator
+
+class DDColor:
+    def __init__(self, device):
+        print(f"Initializing DDColor to {device}")
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        model_id = 'damo/cv_ddcolor_image-colorization'
+        self.pipeline_colorization = pipeline(Tasks.image_colorization, model=model_id)
+
+    @prompts(name="DDColor (Image Colorization)",
+             description="Useful when you make a gray image into color. Receives image_path as input. "
+                         "The input to this tool should be a string, representing the image_path. ")
+    def inference(self, image_path):
+
+        image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+        image_path = image_path.strip("\n")
+        result =  self.pipeline_colorization(image_path)
+        cv2.imwrite(image_filename, result[OutputKeys.OUTPUT_IMG])
+        return image_filename
+    
+
+class UNet:
+    def __init__(self, device):
+        print(f"Initializing UNet to {device}")
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        model_id = 'damo/cv_unet_image-colorization'
+        self.pipeline_colorization = pipeline(Tasks.image_colorization, model=model_id)
+
+    @prompts(name="UNet (Image Colorization)",
+             description="Useful when you make a gray image into color. Receives image_path as input. "
+                         "The input to this tool should be a string, representing the image_path. ")
+    def inference(self, image_path):
+
+        image_filename = f"{str(uuid.uuid4())[:8]}.png"#os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+        image_path = image_path.strip("\n")
+        result =  self.pipeline_colorization(image_path)
+        cv2.imwrite(image_filename, result['output_img'])
+        return image_filename
+    
+if __name__ == "__main__":
+    color_model = UNet(device="cuda:0")
+    docs = color_model.inference("WechatIMG893.jpeg")
+    print(docs)
