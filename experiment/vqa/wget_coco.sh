c3a2bf8af18a5c6cd87c3f140688b756a9c8bc78
Mingchen <64179323+mczhuge@users.noreply.github.com>
2023-05-24 16:23:32 +0300

    Update wget_coco.sh

--- experiment/vqa/wget_coco.sh
+++ experiment/vqa/wget_coco.sh
@@ -9,4 +9,4 @@ for split in val; do
 done
 
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
-unzip annotations_trainval2017.zip -d ${COCO_DIR}; rm annotations_trainval2017.zip
+unzip annotations_trainval2017.zip; rm annotations_trainval2017.zip
