0c21c2fd2f40d338e16f93daae21afd4110efdfc
Mingchen <64179323+mczhuge@users.noreply.github.com>
2023-05-24 16:16:37 +0300

    Update wget_coco.sh

--- experiment/vqa/wget_coco.sh
+++ experiment/vqa/wget_coco.sh
@@ -5,7 +5,7 @@ cd coco
 #for split in train val test; do
 for split in val; do
     wget "http://images.cocodataset.org/zips/${split}2017.zip"
-    unzip "${split}2017.zip" -d 
+    unzip "${split}2017.zip"
 done
 
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
