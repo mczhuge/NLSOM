9cd8a11f145b632e359b803d875e16b264b26ed1
mczhuge <mczhuge@gmail.com>
2023-05-29 00:41:36 +0300

    update

--- society/ocr/agent.py
+++ society/ocr/agent.py
@@ -1,6 +1,3 @@
-import requests
-import torch
-import os
 import easyocr
 
 def prompts(name, description):
@@ -15,7 +12,7 @@ class EasyOCR:
     def __init__(self, device="cpu"):
         self.ocr = easyocr.Reader(['ch_sim','en']) 
 
-    @prompts(name="EasyOCR (OCR)",
+    @prompts(name="EasyOCR",
              description="useful when you want to recognize the word or text in an image. "
                          "The input to this tool should be a string, representing the image_path. ")
     def inference(self, filename):
@@ -25,7 +22,6 @@ class EasyOCR:
         for item in result:
             text += " " + item[1]
         return text.strip()
-
 
 
 if __name__ == "__main__":
