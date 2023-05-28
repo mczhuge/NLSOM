99607e3d520a89d31bf04f29c97e8f9d99d4a32c
mczhuge <mczhuge@gmail.com>
2023-05-29 01:56:33 +0300

    update

--- society/image_colorization/agent.py
+++ society/image_colorization/agent.py
@@ -2,7 +2,6 @@ from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
 from modelscope.outputs import OutputKeys
 import torch
-from PIL import Image
 import os
 import uuid
 import cv2
@@ -16,45 +15,45 @@ def prompts(name, description):
     return decorator
 
 class DDColor:
-    def __init__(self, device):
-        print(f"Initializing DDColor to {device}")
+    def __init__(self, device="cuda:0"):
         self.device = device
         self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
         model_id = 'damo/cv_ddcolor_image-colorization'
         self.pipeline_colorization = pipeline(Tasks.image_colorization, model=model_id)
 
-    @prompts(name="DDColor (Image Colorization)",
+    @prompts(name="DDColor",
              description="Useful when you make a gray image into color. Receives image_path as input. "
                          "The input to this tool should be a string, representing the image_path. ")
     def inference(self, image_path):
 
         image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+        #image_filename = "result.png"
         image_path = image_path.strip("\n")
         result =  self.pipeline_colorization(image_path)
         cv2.imwrite(image_filename, result[OutputKeys.OUTPUT_IMG])
         return image_filename
     
 
-class UNet:
-    def __init__(self, device):
-        print(f"Initializing UNet to {device}")
-        self.device = device
-        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
-        model_id = 'damo/cv_unet_image-colorization'
-        self.pipeline_colorization = pipeline(Tasks.image_colorization, model=model_id)
+# class UNet:
+#     def __init__(self, device):
+#         print(f"Initializing UNet to {device}")
+#         self.device = device
+#         self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+#         model_id = 'damo/cv_unet_image-colorization'
+#         self.pipeline_colorization = pipeline(Tasks.image_colorization, model=model_id)
 
-    @prompts(name="UNet (Image Colorization)",
-             description="Useful when you make a gray image into color. Receives image_path as input. "
-                         "The input to this tool should be a string, representing the image_path. ")
-    def inference(self, image_path):
+#     @prompts(name="UNet (Image Colorization)",
+#              description="Useful when you make a gray image into color. Receives image_path as input. "
+#                          "The input to this tool should be a string, representing the image_path. ")
+#     def inference(self, image_path):
 
-        image_filename = f"{str(uuid.uuid4())[:8]}.png"#os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
-        image_path = image_path.strip("\n")
-        result =  self.pipeline_colorization(image_path)
-        cv2.imwrite(image_filename, result['output_img'])
-        return image_filename
+#         image_filename = f"{str(uuid.uuid4())[:8]}.png"#os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+#         image_path = image_path.strip("\n")
+#         result =  self.pipeline_colorization(image_path)
+#         cv2.imwrite(image_filename, result['output_img'])
+#         return image_filename
     
 if __name__ == "__main__":
-    color_model = UNet(device="cuda:0")
-    docs = color_model.inference("WechatIMG893.jpeg")
-    print(docs)
+    color_model = DDColor(device="cuda:0")
+    img = color_model.inference("c20805d012f6f4c9ffee3944b9a10e33_r.jpeg")
+    print(img)
