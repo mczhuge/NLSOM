ce182df08e510ccf95f1323c153d44219c7d518d
mczhuge <mczhuge@gmail.com>
2023-05-29 00:06:57 +0300

    update

--- society/sentence_refine/agent.py
+++ society/sentence_refine/agent.py
@@ -1,13 +1,12 @@
 import os
 import openai
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
 from tenacity import (
     retry,
     stop_after_attempt,
     wait_random_exponential,
 ) 
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
   
 def prompts(name, description):
     def decorator(func):
@@ -22,23 +21,23 @@ class SentenceRefine:
     def __init__(self, device="cuda:0"):
         self.device = device
         
-
     @prompts(name="SentenceRefine",
              description="A wrapper around Microsoft bing.com,"
                          "Useful for when you need to search information from the internet, "
                          "Input should be a search query.")
     def inference(self, text):
-        prompt = "refine this: " + text
+        prompt = "RPlease revise and slightly expand the following sentence: " + text
         self.instructgpt = openai.Completion.create(
                     engine="text-davinci-003",
                     prompt=prompt,
-                    max_tokens=50,
+                    temperature=0.5,
+                    max_tokens=200,
                     )
         answer = self.instructgpt["choices"][0]["text"].strip("\n")
         return answer
 
 if __name__ == "__main__":
     refine= SentenceRefine()
-    ans = refine.inference("Juergen Schmidhuber is a scientist.")
-    # Juergen Schmidhuber is a renowned scientist and computer scientist who has made significant contributions to the fields of artificial intelligence and machine learning.
+    ans = refine.inference("AGI is comming.")
+    # AGI (Artificial General Intelligence) is rapidly approaching, with many experts predicting that it will be achieved within the next few decades.
     print(ans)
