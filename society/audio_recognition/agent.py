38f496246e62dc1922bcd9aa6fc00ad2c8caec52
mczhuge <mczhuge@gmail.com>
2023-05-29 00:53:33 +0300

    update

--- society/audio_recognition/agent.py
+++ society/audio_recognition/agent.py
@@ -1,8 +1,5 @@
 import requests
 from modelscope.pipelines import pipeline
-from modelscope.utils.constant import Tasks
-import torch
-import os
 
 def prompts(name, description):
     def decorator(func):
@@ -13,21 +10,19 @@ def prompts(name, description):
     return decorator
 
 class Whisper:
-    def __init__(self, device):
-        print(f"Initializing Audio2Text to {device}")
-        self.device = "cpu"
+    def __init__(self, device="cpu"):
+        self.device = device
         self.API_URL = "https://api-inference.huggingface.co/models/openai/whisper-base"
         self.headers = {"Authorization": "Bearer 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"} # TODO: Huggingface API
 
 
-    @prompts(name="Whisper (audio-recognition)",
+    @prompts(name="Whisper",
              description="useful when you want to recognize the context of an audio file. "
                          "Whisper is a general-purpose speech recognition model. "
                          "The input to this tool should be a string, representing the text used to generate image. ")
     def inference(self, filename):
 
         audio_json = self.query(filename)
-        # You can access the image with PIL.Image for example
 
         print(
             f"\nProcessed Audio2Text, Input File: {filename}, Output Content: {audio_json}")
@@ -41,25 +36,25 @@ class Whisper:
         return response.json()
     
 
-class Paraformer:
-    def __init__(self, device):
-        print(f"Initializing Paraformer to {device}")
-        self.device = device
-        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
-        model_id = 'damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online'
-        self.pipeline_asr = pipeline('auto-speech-recognition', model=model_id)
+# class Paraformer:
+#     def __init__(self, device="cuda:0"):
+#         self.device = device
+#         model_id = 'damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online'
+#         self.pipeline_asr = pipeline('auto-speech-recognition', model=model_id)
 
-    @prompts(name="Paraformer (Chinese audio-recognition)",
-             description="useful when you want to recognize the context of an audio file. "
-                         "The input to this tool should be a string, representing the image_path. ")
-    def inference(self, filename):
+#     @prompts(name="Paraformer",
+#              description="useful when you want to recognize the Chinese context of a Chinese audio file. "
+#                          "The input to this tool should be a string, representing the image_path. ")
+#     def inference(self, filename):
 
-        result =  self.pipeline_asr(filename)
-        return result
+#         result =  self.pipeline_asr(filename)
+#         return result
     
 
-
 if __name__ == "__main__":
-    asr_model = Paraformer(device="cuda:0")
-    result = asr_model.inference("http://www.modelscope.cn/api/v1/models/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online/repo?Revision=master\u0026FilePath=example/asr_example.wav")
+    #"http://www.modelscope.cn/api/v1/models/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online/repo?Revision=master\u0026FilePath=example/asr_example.wav"
+    
+    asr_model = Whisper()
+    #asr_model = Paraformer(device="cuda:0")
+    result = asr_model.inference("sample1.flac")
     print(result)
