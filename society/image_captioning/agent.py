c7334e2b3eec7bad35033a108e3877ef8eb00a4c
mczhuge <mczhuge@gmail.com>
2023-05-29 14:56:56 +0300

    update

--- society/image_captioning/agent.py
+++ society/image_captioning/agent.py
@@ -16,8 +16,31 @@ def prompts(name, description):
 
     return decorator
 
+class OFA_large_captioning:
+    def __init__(self, device="cuda:0"):
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        model_id = 'damo/ofa_image-caption_coco_large_en'
+        self.pipeline_caption = pipeline(Tasks.image_captioning, model=model_id, model_revision='v1.0.1')
+
+    @prompts(name="OFA_large_captioning",
+             description="Useful when you want to know what is inside the photo. Receives image_path as input. "
+                         "If there are other captioning methods, it is also suggested to utilize other captioning methods to better know the image."
+                         "The input to this tool should be a string, representing the image_path. ")
+    def inference(self, image_path):
+        image_path = image_path.strip("\n")
+        captions = self.pipeline_caption(image_path)[OutputKeys.CAPTION]
+        return captions[0]
+
+
+if __name__ == "__main__":
+    ic = BLIP2_captioning("cuda:0")
+    desc = ic.inference("d317f96a.png")
+
+# You can add these 3 candidates
+"""
 class mPLUG_captioning:
-    def __init__(self, device):
+    def __init__(self, device="cuda:0"):
         self.device = device
         self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
         model_id = 'damo/mplug_image-captioning_coco_large_en'
@@ -31,10 +54,11 @@ class mPLUG_captioning:
         image_path = image_path.strip("\n")
         captions = self.pipeline_caption(image_path)
         print(f"\nProcessed mPLUG_captioning, Input Image: {image_path}, Output Text: {captions}")
-        return captions
-    
+        return captions["caption"]
+
+
 class OFA_distilled_captioning:
-    def __init__(self, device):
+    def __init__(self, device="cuda:0"):
         self.device = device
         self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
         model_id = 'damo/ofa_image-caption_coco_large_en'
@@ -48,27 +72,10 @@ class OFA_distilled_captioning:
         image_path = image_path.strip("\n")
         captions = self.pipeline_caption(image_path)[OutputKeys.CAPTION]
         return captions
-    
-class OFA_large_captioning:
-    def __init__(self, device):
-        self.device = device
-        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
-        model_id = 'damo/ofa_image-caption_coco_large_en'
-        self.pipeline_caption = pipeline(Tasks.image_captioning, model=model_id, model_revision='v1.0.1')
 
-    @prompts(name="OFA_large_captioning",
-             description="Useful when you want to know what is inside the photo. Receives image_path as input. "
-                         "If there are other captioning methods, it is also suggested to utilize other captioning methods to better know the image."
-                         "The input to this tool should be a string, representing the image_path. ")
-    def inference(self, image_path):
-        image_path = image_path.strip("\n")
-        captions = self.pipeline_caption(image_path)[OutputKeys.CAPTION]
-        return captions
-    
 class BLIP2_captioning:
-    def __init__(self, device):
+    def __init__(self, device="cuda:0"):
         self.device = device
-        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
         model_id = 'Salesforce/blip2-flan-t5-xl'
         self.processor = Blip2Processor.from_pretrained(model_id)
         self.BLIP2_MODEL = Blip2ForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16)
@@ -85,4 +92,4 @@ class BLIP2_captioning:
         answer = self.processor.batch_decode(generated_answer_ids, skip_special_tokens=True)[0].strip()
 
         return answer
-
+"""
