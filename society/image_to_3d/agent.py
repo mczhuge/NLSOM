4d6bf9c948b6c460c374e35c9aa64ce6d2c8c2ef
mczhuge <mczhuge@gmail.com>
2023-05-29 18:58:20 +0300

    update

--- society/image_to_3d/agent.py
+++ society/image_to_3d/agent.py
@@ -1,11 +1,11 @@
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
 from modelscope.outputs import OutputKeys
-import torch
-from PIL import Image
 import os
 import uuid
-import cv2
+import urllib.request
+import shutil
+
 
 def prompts(name, description):
     def decorator(func):
@@ -16,27 +16,26 @@ def prompts(name, description):
     return decorator
 
 
-class HumanReconstruction:
-    def __init__(self, device):
-        print(f"Initializing HRNet to {device}")
-        self.device = device
-        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+class HRNet:
+    def __init__(self, device="cuda:0"):
         model_id = 'damo/cv_hrnet_image-human-reconstruction'
         self.human_reconstruction_pipeline = pipeline(Tasks.human_reconstruction, model=model_id)
 
-    @prompts(name="HumanReconstruction",
+    @prompts(name="HRNet",
              description="Useful when you turn a personal photo into 3D mesh. Receives image_path as input. "
                          "The input to this tool should be a string, representing the image_path. ")
     def inference(self, image_path):
 
-        image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+        color_filename = os.path.join('data', f"human_color_{str(uuid.uuid4())[:8]}.obj")
+        recon_filename = os.path.join('data', f"human_reconstruction_{str(uuid.uuid4())[:8]}.obj")
         image_path = image_path.strip("\n")
         result =  self.human_reconstruction_pipeline(image_path)
-        #cv2.imwrite(image_filename, result[OutputKeys.OUTPUT_IMG])
         mesh = result[OutputKeys.OUTPUT]
-        return mesh
+        shutil.move("human_color.obj", color_filename)
+        shutil.move("human_reconstruction.obj", recon_filename)
+        return recon_filename
     
 if __name__ == "__main__":
-    hr_model = HumanReconstruction(device="cuda:0")
-    docs = hr_model.inference("WechatIMG899.jpeg")
+    hr_model = HRNet(device="cuda:0")
+    docs = hr_model.inference("data/WechatIMG899.jpeg")
     print(docs)
