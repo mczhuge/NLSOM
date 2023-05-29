267f2633c689f2821700a89da7330accde8dd704
mczhuge <mczhuge@gmail.com>
2023-05-29 20:43:28 +0300

    update

--- society/text_to_speech/agent.py
+++ society/text_to_speech/agent.py
@@ -1,4 +1,4 @@
-
+# https://github.com/coqui-ai/TTS
 import uuid
 import os
 import io
@@ -14,26 +14,26 @@ def prompts(name, description):
 
     return decorator
 
-class Text2Speech:
+class TTS:
     def __init__(self, device):
-        print(f"Initializing Text2Speech to {device}")
         self.device = device
         self.torch_dtype = "cpu"
         model_name = TTS.list_models()[0]
         self.tts = TTS(model_name)
 
 
-    @prompts(name="Text2Speech (text-to-speech)",
+    @prompts(name="TTS",
              description="useful when you want to generate an audio from a user input text and save it to a file. "
                          "The input to this tool should be a string, representing the text used to generate audio. ")
     def inference(self, text):
         
         audio_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.wav")
+        #audio_filename = f"{str(uuid.uuid4())[:8]}.wav"
 
         self.tts.tts_to_file(text=text, speaker=self.tts.speakers[0], language=self.tts.languages[0], file_path=audio_filename)
 
         return audio_filename
 
 if __name__ == "__main__":
-    tts_model = Text2Speech(device="cuda:0")
-    image = tts_model.inference("I love you! I love you! I love you! I love you!")
+    tts_model = TTS(device="cuda:0")
+    image = tts_model.inference("Wo zong shi lin shi bao fo jiao, lin shi bao fo jiao.")
