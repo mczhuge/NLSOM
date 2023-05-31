2eeb691ff004b434fede76dcbaa27f705aa85cda
mczhuge <mczhuge@gmail.com>
2023-05-31 14:28:01 +0300

    update

--- society/text_to_image/agent.py
+++ society/text_to_image/agent.py
@@ -17,7 +17,7 @@ class AnythingV4:
     def __init__(self, device="cpu"):
         self.device = device
         self.API_URL = "https://api-inference.huggingface.co/models/andite/anything-v4.0"
-        self.headers = {"Authorization": "Bearer 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"}
+        self.headers = {"Authorization": "Bearer "+os.getenv("HUGGINGFACE_ACCESS_Tokens")}
 
         self.a_prompt = 'best quality, extremely detailed'
 
@@ -43,188 +43,194 @@ class AnythingV4:
         response = requests.post(self.API_URL, headers=self.headers, json=payload)
         return response.content
     
-class AnythingV3:
-    def __init__(self, device="cpu"):
-        self.device = device
-        self.API_URL = "https://api-inference.huggingface.co/models/Linaqruf/anything-v3.0"
-        self.headers = {"Authorization": "Bearer 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"}
-
-        self.a_prompt = 'best quality, extremely detailed'
-
-    @prompts(name="AnythingV3",
-             description="useful when you want to generate an image from a user input text and save it to a file. "
-                         "like: generate an image of an object or something, or generate an image that includes some objects. "
-                         "The input to this tool should be a string, representing the text used to generate image. ")
-    def inference(self, text):
-        
-        image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
-        #image_filename = "result.png"
-        prompt = str(text + ', ' + self.a_prompt)
-
-        image_bytes = self.query({
-	            "inputs": prompt,
-            })
-
-        image = Image.open(io.BytesIO(image_bytes))
-        image.save(image_filename)
-        return image_filename
-
-    def query(self, payload):
-        response = requests.post(self.API_URL, headers=self.headers, json=payload)
-        return response.content
     
-class OpenJourneyV4:
-    def __init__(self, device="cpu"):
-        self.device = device
-        self.API_URL = "https://api-inference.huggingface.co/models/prompthero/openjourney-v4"
-        self.headers = {"Authorization": "Bearer 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"}
+# class AnythingV3:
+#     def __init__(self, device="cpu"):
+#         self.device = device
+#         self.API_URL = "https://api-inference.huggingface.co/models/Linaqruf/anything-v3.0"
+#         self.headers = {"Authorization": "Bearer "+os.getenv("HUGGINGFACE_ACCESS_Tokens")}
 
-        self.a_prompt = 'best quality, extremely detailed'
+#         self.a_prompt = 'best quality, extremely detailed'
 
-    @prompts(name="OpenJourneyV4",
-             description="useful when you want to generate an image from a user input text and save it to a file. "
-                         "like: generate an image of an object or something, or generate an image that includes some objects. "
-                         "The input to this tool should be a string, representing the text used to generate image. ")
-    def inference(self, text):
+#     @prompts(name="AnythingV3",
+#              description="useful when you want to generate an image from a user input text and save it to a file. "
+#                          "like: generate an image of an object or something, or generate an image that includes some objects. "
+#                          "The input to this tool should be a string, representing the text used to generate image. ")
+#     def inference(self, text):
         
-        image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
-        #image_filename = "result.png"
-        prompt = str(text + ', ' + self.a_prompt)
+#         image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+#         #image_filename = "result.png"
+#         prompt = str(text + ', ' + self.a_prompt)
 
-        image_bytes = self.query({
-	            "inputs": prompt,
-            })
+#         image_bytes = self.query({
+# 	            "inputs": prompt,
+#             })
 
-        image = Image.open(io.BytesIO(image_bytes))
-        image.save(image_filename)
-        return image_filename
+#         image = Image.open(io.BytesIO(image_bytes))
+#         image.save(image_filename)
+#         return image_filename
 
-    def query(self, payload):
-        response = requests.post(self.API_URL, headers=self.headers, json=payload)
-        return response.content
+#     def query(self, payload):
+#         response = requests.post(self.API_URL, headers=self.headers, json=payload)
+#         return response.content
     
-class OpenJourney:
-    def __init__(self, device="cpu"):
-        self.device = device
-        self.API_URL = "https://api-inference.huggingface.co/models/prompthero/openjourney"
-        self.headers = {"Authorization": "Bearer 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"}
-
-        self.a_prompt = 'best quality, extremely detailed'
-
-    @prompts(name="OpenJourney",
-             description="useful when you want to generate an image from a user input text and save it to a file. "
-                         "like: generate an image of an object or something, or generate an image that includes some objects. "
-                         "The input to this tool should be a string, representing the text used to generate image. ")
-    def inference(self, text):
-        
-        image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
-        #image_filename = "result.png"
-        prompt = str(text + ', ' + self.a_prompt)
-
-        image_bytes = self.query({
-	            "inputs": prompt,
-            })
-
-        image = Image.open(io.BytesIO(image_bytes))
-        image.save(image_filename)
-        return image_filename
-
-    def query(self, payload):
-        response = requests.post(self.API_URL, headers=self.headers, json=payload)
-        return response.content
     
-class StableDiffusionV15:
-    def __init__(self, device="cpu"):
-        self.device = device
-        self.API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
-        self.headers = {"Authorization": "Bearer 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"}
+# class OpenJourneyV4:
+#     def __init__(self, device="cpu"):
+#         self.device = device
+#         self.API_URL = "https://api-inference.huggingface.co/models/prompthero/openjourney-v4"
+#         self.headers = {"Authorization": "Bearer "+os.getenv("HUGGINGFACE_ACCESS_Tokens")}
 
-        self.a_prompt = 'best quality, extremely detailed'
+#         self.a_prompt = 'best quality, extremely detailed'
 
-    @prompts(name="StableDiffusionV15",
-             description="useful when you want to generate an image from a user input text and save it to a file. "
-                         "like: generate an image of an object or something, or generate an image that includes some objects. "
-                         "The input to this tool should be a string, representing the text used to generate image. ")
-    def inference(self, text):
+#     @prompts(name="OpenJourneyV4",
+#              description="useful when you want to generate an image from a user input text and save it to a file. "
+#                          "like: generate an image of an object or something, or generate an image that includes some objects. "
+#                          "The input to this tool should be a string, representing the text used to generate image. ")
+#     def inference(self, text):
         
-        image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
-        #image_filename = "result.png"
-        prompt = str(text + ', ' + self.a_prompt)
+#         image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+#         #image_filename = "result.png"
+#         prompt = str(text + ', ' + self.a_prompt)
 
-        image_bytes = self.query({
-	            "inputs": prompt,
-            })
+#         image_bytes = self.query({
+# 	            "inputs": prompt,
+#             })
 
-        image = Image.open(io.BytesIO(image_bytes))
-        image.save(image_filename)
-        return image_filename
+#         image = Image.open(io.BytesIO(image_bytes))
+#         image.save(image_filename)
+#         return image_filename
 
-    def query(self, payload):
-        response = requests.post(self.API_URL, headers=self.headers, json=payload)
-        return response.content
+#     def query(self, payload):
+#         response = requests.post(self.API_URL, headers=self.headers, json=payload)
+#         return response.content
+    
+    
+# class OpenJourney:
+#     def __init__(self, device="cpu"):
+#         self.device = device
+#         self.API_URL = "https://api-inference.huggingface.co/models/prompthero/openjourney"
+#         self.headers = {"Authorization": "Bearer "+os.getenv("HUGGINGFACE_ACCESS_Tokens")}
 
-class StableDiffusionV21B:
-    def __init__(self, device="cpu"):
-        self.device = device
-        self.API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1-base"
-        self.headers = {"Authorization": "Bearer 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"}
+#         self.a_prompt = 'best quality, extremely detailed'
 
-        self.a_prompt = 'best quality, extremely detailed'
-
-    @prompts(name="StableDiffusionV21B",
-             description="useful when you want to generate an image from a user input text and save it to a file. "
-                         "like: generate an image of an object or something, or generate an image that includes some objects. "
-                         "The input to this tool should be a string, representing the text used to generate image. ")
-    def inference(self, text):
+#     @prompts(name="OpenJourney",
+#              description="useful when you want to generate an image from a user input text and save it to a file. "
+#                          "like: generate an image of an object or something, or generate an image that includes some objects. "
+#                          "The input to this tool should be a string, representing the text used to generate image. ")
+#     def inference(self, text):
         
-        image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
-        #image_filename = "result.png"
-        prompt = str(text + ', ' + self.a_prompt)
+#         image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+#         #image_filename = "result.png"
+#         prompt = str(text + ', ' + self.a_prompt)
 
-        image_bytes = self.query({
-	            "inputs": prompt,
-            })
+#         image_bytes = self.query({
+# 	            "inputs": prompt,
+#             })
 
-        image = Image.open(io.BytesIO(image_bytes))
-        image.save(image_filename)
-        return image_filename
+#         image = Image.open(io.BytesIO(image_bytes))
+#         image.save(image_filename)
+#         return image_filename
 
-    def query(self, payload):
-        response = requests.post(self.API_URL, headers=self.headers, json=payload)
-        return response.content
+#     def query(self, payload):
+#         response = requests.post(self.API_URL, headers=self.headers, json=payload)
+#         return response.content
+    
+    
+# class StableDiffusionV15:
+#     def __init__(self, device="cpu"):
+#         self.device = device
+#         self.API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+#         self.headers = {"Authorization": "Bearer "+os.getenv("HUGGINGFACE_ACCESS_Tokens")}
 
-class StableDiffusionV21:
-    def __init__(self, device="cpu"):
-        self.device = device
-        self.API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1"
-        self.headers = {"Authorization": "Bearer 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"}
+#         self.a_prompt = 'best quality, extremely detailed'
 
-        self.a_prompt = 'best quality, extremely detailed'
-
-    @prompts(name="StableDiffusionV21",
-             description="useful when you want to generate an image from a user input text and save it to a file. "
-                         "like: generate an image of an object or something, or generate an image that includes some objects. "
-                         "The input to this tool should be a string, representing the text used to generate image. ")
-    def inference(self, text):
+#     @prompts(name="StableDiffusionV15",
+#              description="useful when you want to generate an image from a user input text and save it to a file. "
+#                          "like: generate an image of an object or something, or generate an image that includes some objects. "
+#                          "The input to this tool should be a string, representing the text used to generate image. ")
+#     def inference(self, text):
         
-        image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
-        #image_filename = "result.png"
-        prompt = str(text + ', ' + self.a_prompt)
+#         image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+#         #image_filename = "result.png"
+#         prompt = str(text + ', ' + self.a_prompt)
 
-        image_bytes = self.query({
-	            "inputs": prompt,
-            })
+#         image_bytes = self.query({
+# 	            "inputs": prompt,
+#             })
 
-        image = Image.open(io.BytesIO(image_bytes))
-        image.save(image_filename)
-        return image_filename
+#         image = Image.open(io.BytesIO(image_bytes))
+#         image.save(image_filename)
+#         return image_filename
 
-    def query(self, payload):
-        response = requests.post(self.API_URL, headers=self.headers, json=payload)
-        return response.content
+#     def query(self, payload):
+#         response = requests.post(self.API_URL, headers=self.headers, json=payload)
+#         return response.content
+
+
+# class StableDiffusionV21B:
+#     def __init__(self, device="cpu"):
+#         self.device = device
+#         self.API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1-base"
+#         self.headers = {"Authorization": "Bearer "+os.getenv("HUGGINGFACE_ACCESS_Tokens")}
+
+#         self.a_prompt = 'best quality, extremely detailed'
+
+#     @prompts(name="StableDiffusionV21B",
+#              description="useful when you want to generate an image from a user input text and save it to a file. "
+#                          "like: generate an image of an object or something, or generate an image that includes some objects. "
+#                          "The input to this tool should be a string, representing the text used to generate image. ")
+#     def inference(self, text):
+        
+#         image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+#         #image_filename = "result.png"
+#         prompt = str(text + ', ' + self.a_prompt)
+
+#         image_bytes = self.query({
+# 	            "inputs": prompt,
+#             })
+
+#         image = Image.open(io.BytesIO(image_bytes))
+#         image.save(image_filename)
+#         return image_filename
+
+#     def query(self, payload):
+#         response = requests.post(self.API_URL, headers=self.headers, json=payload)
+#         return response.content
+
+
+# class StableDiffusionV21:
+#     def __init__(self, device="cpu"):
+#         self.device = device
+#         self.API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1"
+#         self.headers = {"Authorization": "Bearer "+os.getenv("HUGGINGFACE_ACCESS_Tokens")}
+
+#         self.a_prompt = 'best quality, extremely detailed'
+
+#     @prompts(name="StableDiffusionV21",
+#              description="useful when you want to generate an image from a user input text and save it to a file. "
+#                          "like: generate an image of an object or something, or generate an image that includes some objects. "
+#                          "The input to this tool should be a string, representing the text used to generate image. ")
+#     def inference(self, text):
+        
+#         image_filename = os.path.join('data', f"{str(uuid.uuid4())[:8]}.png")
+#         #image_filename = "result.png"
+#         prompt = str(text + ', ' + self.a_prompt)
+
+#         image_bytes = self.query({
+# 	            "inputs": prompt,
+#             })
+
+#         image = Image.open(io.BytesIO(image_bytes))
+#         image.save(image_filename)
+#         return image_filename
+
+#     def query(self, payload):
+#         response = requests.post(self.API_URL, headers=self.headers, json=payload)
+#         return response.content
 
 
 if __name__ == "__main__":
      
-     t2i = StableDiffusionV21()
-     t2i.inference("Chinese Uncle in the Old Street.")
+     t2i = AnythingV4()
+     t2i.inference("Chinese SuperMan in the Old Street.")
